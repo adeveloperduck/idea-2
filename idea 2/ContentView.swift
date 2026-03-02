c3d570bb617fb05d3d4af60e6c47df03ca4617a6
adeveloperduck <navneet.dagdiya@student.education.wa.edu.au>
2026-03-02 14:56:23 +0800

    Initial Commit

---
+++ idea 2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  idea 2
+//
+//  Created by Navneet Dagdiya on 2/3/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ idea 2/idea_2App.swift
@@ -0,0 +1,17 @@
+//
+//  idea_2App.swift
+//  idea 2
+//
+//  Created by Navneet Dagdiya on 2/3/2026.
+//
+
+import SwiftUI
+
+@main
+struct idea_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
